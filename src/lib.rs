ddc68e526ed5aa9e650fd7fac5abc04ec7c27b96
darkwater4213 <53630002+darkwater4213@users.noreply.github.com>
2023-04-13 10:12:41 -0700

    No errors!!! (but plenty of warnings)

--- src/lib.rs
+++ src/lib.rs
@@ -1,53 +1,65 @@
-pub static TAB: &str = "    ";
-pub static ESC: &str = "\u{1b}";
+#[allow(unused)]
+pub(crate) static TAB: &str = "    ";
+#[allow(unused)]
+pub(crate) static ESC: &str = "\u{1b}";
 
-pub mod RESET {
-	pub static ALL: &str	= "\u{1b}[0m";
-	pub static COLOR: &str	= "\u{1b}[49m";
-	pub static BOLD: &str	= "\u{1b}[21m";
-	pub static DIM: &str	= "\u{1b}[22m";
-	pub static ITALIC: &str	= "\u{1b}[23m";
-	pub static UL: &str		= "\u{1b}[24m";
+#[allow(unused)]
+#[allow(non_snake_case)]
+pub(crate) mod RESET {
+	pub(crate) static ALL: &str	= "\u{1b}[0m";
+	pub(crate) static COLOR: &str	= "\u{1b}[49m";
+	pub(crate) static BOLD: &str	= "\u{1b}[21m";
+	pub(crate) static DIM: &str	= "\u{1b}[22m";
+	pub(crate) static ITALIC: &str	= "\u{1b}[23m";
+	pub(crate) static UL: &str		= "\u{1b}[24m";
 }
 
-pub mod COLOR {
-	pub static BLACK: &str	= "\u{1b}[30m";
-	pub static RED: &str	= "\u{1b}[31m";
-	pub static GREEN: &str	= "\u{1b}[32m";
-	pub static YELLOW: &str	= "\u{1b}[33m";
-	pub static BLUE: &str	= "\u{1b}[34m";
-	pub static PURPLE: &str	= "\u{1b}[35m";
-	pub static CYAN: &str	= "\u{1b}[36m";
-	pub static WHITE: &str	= "\u{1b}[37m";
+#[allow(unused)]
+#[allow(non_snake_case)]
+pub(crate) mod COLOR {
+	pub(crate) static BLACK: &str	= "\u{1b}[30m";
+	pub(crate) static RED: &str	= "\u{1b}[31m";
+	pub(crate) static GREEN: &str	= "\u{1b}[32m";
+	pub(crate) static YELLOW: &str	= "\u{1b}[33m";
+	pub(crate) static BLUE: &str	= "\u{1b}[34m";
+	pub(crate) static PURPLE: &str	= "\u{1b}[35m";
+	pub(crate) static CYAN: &str	= "\u{1b}[36m";
+	pub(crate) static WHITE: &str	= "\u{1b}[37m";
 }
 
-pub mod BRIGHT {
-	pub static BLACK: &str	= "\u{1b}[90m";
-	pub static RED: &str	= "\u{1b}[91m";
-	pub static GREEN: &str	= "\u{1b}[92m";
-	pub static YELLOW: &str	= "\u{1b}[93m";
-	pub static BLUE: &str	= "\u{1b}[94m";
-	pub static PURPLE: &str	= "\u{1b}[95m";
-	pub static CYAN: &str	= "\u{1b}[96m";
-	pub static WHITE: &str	= "\u{1b}[97m";
+#[allow(unused)]
+#[allow(non_snake_case)]
+pub(crate) mod BRIGHT {
+	pub(crate) static BLACK: &str	= "\u{1b}[90m";
+	pub(crate) static RED: &str	= "\u{1b}[91m";
+	pub(crate) static GREEN: &str	= "\u{1b}[92m";
+	pub(crate) static YELLOW: &str	= "\u{1b}[93m";
+	pub(crate) static BLUE: &str	= "\u{1b}[94m";
+	pub(crate) static PURPLE: &str	= "\u{1b}[95m";
+	pub(crate) static CYAN: &str	= "\u{1b}[96m";
+	pub(crate) static WHITE: &str	= "\u{1b}[97m";
 }
 
-pub mod STYLE {
-	pub static DIR: &str		= "\u{1b}[0;1;94m";
-	pub static FILE: &str		= "\u{1b}[0;1;97m";
-	pub static EXEC: &str		= "\u{1b}[0;1;92m";
-	pub static SYMLINK: &str	= "\u{1b}[0;1;96m";
-	// pub static NEWLINE: &str	= RESET + "\n";
+#[allow(unused)]
+#[allow(non_snake_case)]
+pub(crate) mod STYLE {
+	pub(crate) static DIR: &str		= "\u{1b}[0;1;94m";
+	pub(crate) static FILE: &str		= "\u{1b}[0;1;97m";
+	pub(crate) static EXEC: &str		= "\u{1b}[0;1;92m";
+	pub(crate) static SYMLINK: &str	= "\u{1b}[0;1;96m";
 
-	pub static BOLD: &str	= "\u{1b}[1m";
-	pub static DIM: &str	= "\u{1b}[2m";
-	pub static ITALIC: &str	= "\u{1b}[3m";
-	pub static UL: &str		= "\u{1b}[4m";
+	pub(crate) static BOLD: &str	= "\u{1b}[1m";
+	pub(crate) static DIM: &str	= "\u{1b}[2m";
+	pub(crate) static ITALIC: &str	= "\u{1b}[3m";
+	pub(crate) static UL: &str		= "\u{1b}[4m";
 }
 
 mod types {
 
-	// TODO: impl std::fmt::Display for FileName<&str> {}
+	// TODO: finish the impl below
+	// impl std::fmt::Display for FileName<&str> {
+	// 	fn fmt(&self, _: &mut Formatter) -> Result<(), std::fmt::Error> { todo!() }
+	// }
 	// #[derive(Display)]
 	pub enum FileName<T> {
 		Regular(T),
@@ -58,113 +70,113 @@ mod types {
 	}
 
 	// TODO: create an expandable tuple struct or similar (vector?)
-	// TODO: for composing FileName<&str> to a PathName
+	// TODO: for composing FileName<String> to a PathName
 
-	impl FileName<&str> {
-		fn to_str(self) -> &'static str {
+	impl FileName<String> {
+		pub fn to_string(self) -> String {
 			use FileName::*;
 			use super::STYLE::*;
-			use super::RESET::ALL as RESET;
-			// let mut result: &str;
-			let result = &*match self {
-				Regular(inner) => FILE.to_string() + inner + RESET,
-				Executable(inner) => EXEC.to_string() + inner + RESET,
-				Directory(inner) => DIR.to_string() + inner + RESET,
-				Symlink(inner) => SYMLINK.to_string() + inner + RESET,
-				Pathname(inner) => inner.to_string(),
+			let mut result: String = String::new();
+			result = match self {
+				Regular(inner) => result + FILE + &inner,
+				Executable(inner) => result + EXEC + &inner,
+				Directory(inner) => result + DIR + &inner,
+				Symlink(inner) => result + SYMLINK + &inner,
+				Pathname(inner) => result + "" + &inner,
 			};
-			result
+			result + super::RESET::ALL
 		}
 	}
 
 	trait ToFileName {
-		fn to_filename(&self) -> self::FileName<&str>;
-		fn to_exec_name(&self) -> self::FileName<&str>;
-		fn to_directory(&self) -> self::FileName<&str>;
-		fn to_symlink(&self, target: FileName<&str>) -> self::FileName<&str>;
+		fn to_filename(&self) -> self::FileName<String>;
+		fn to_exec_name(&self) -> self::FileName<String>;
+		fn to_directory(&self) -> self::FileName<String>;
+		fn to_symlink(&self, target: FileName<String>) -> self::FileName<String>;
 	}
 
 	trait ToPathName {
-		fn to_pathname(&self) -> self::FileName<&str>;
+		fn to_pathname(&self) -> self::FileName<String>;
 	}
 
-	impl ToFileName for &str {
-		fn to_filename(&self) -> self::FileName<&str> {
+	impl ToFileName for String {
+		fn to_filename(&self) -> self::FileName<String> {
 			let inner = super::STYLE::FILE.to_string() + &self + super::RESET::ALL;
-			let result = FileName::Regular(&*inner);
+			let result = FileName::Regular(inner);
 			result
 		}
 
-		fn to_exec_name(&self) -> self::FileName<&str> {
+		fn to_exec_name(&self) -> self::FileName<String> {
 			let inner = super::STYLE::EXEC.to_string() + &self + super::RESET::ALL;
-			let result = FileName::Executable(&*inner);
+			let result = FileName::Executable(inner);
 			result
 		}
 
-		fn to_directory(&self) -> self::FileName<&str> {
+		fn to_directory(&self) -> self::FileName<String> {
 			let inner = super::STYLE::DIR.to_string() + &self +
-				match &self.chars().last().expect("Converting &str to FileName::Directory failed!") {
+				match &self.chars().last().expect("Converting String to FileName::Directory failed!") {
 					'/'	=> "",
 					_	=> "/",
 				}
 				+ super::RESET::ALL;
-			let result = FileName::Directory(&*inner);
+			let result = FileName::Directory(inner);
 			result
 		}
 
-		fn to_symlink(&self, target: FileName<&str>) -> self::FileName<&str> {
+		fn to_symlink(&self, target: FileName<String>) -> self::FileName<String> {
 			let inner = super::STYLE::SYMLINK.to_string() +
 				// if ! target exists { super::COLOR::RED } else { "" } +
-				*self + super::RESET::ALL +
-				" -> " + target.to_str() + super::RESET::ALL;
-			let result = FileName::Symlink(&*inner);
+				&*self + super::RESET::ALL +
+				" -> " + &target.to_string() + super::RESET::ALL;
+			let result = FileName::Symlink(inner);
 			result
 		}
 	}
 
 	// TODO: fix the impl below
 	// TODO: check that only the last FileName is NOT a FileName::Directory
-	// impl ToPathName for FileName<&str> {
-	// 	fn to_pathname(&self) -> FileName<&str> {
-	// 		let mut inner: String = (&self).to_str().to_string();
+	// impl ToPathName for FileName<String> {
+	// 	fn to_pathname(&self) -> FileName<String> {
+	// 		let mut inner: String = (&self).to_string().to_string();
 	// 		for i in [ 1 ..= inner.len() - 1 ] {
 	// 			inner.push(self[i]);
 	// 		}
-	// 		let result = FileName::Pathname(&*inner);
+	// 		let result = FileName::Pathname(inner);
 	// 		result
 	// 	}
 	// }
 
-	pub struct LogEntry<'a> {
-		pub text: &'a str,
-		pub direct: FileName<&'a str>,
-		pub infix: &'a str,
-		pub indirect: FileName<&'a str>,
-		pub postfix: &'a str,
+	pub struct LogEntry {
+		pub text: String,
+		pub direct: FileName<String>,
+		pub infix: String,
+		pub indirect: FileName<String>,
+		pub postfix: String,
 	}
 
-	pub enum LogType<'a> {
-		Header(LogEntry<'a>),
-		Info(LogEntry<'a>),
-		Hint(LogEntry<'a>),
-		Message(LogEntry<'a>),
-		Action(LogEntry<'a>),
-		Warning(LogEntry<'a>),
-		Error(LogEntry<'a>),
-		Prompt(LogEntry<'a>)
+	pub enum LogType {
+		Header(LogEntry),
+		Info(LogEntry),
+		Hint(LogEntry),
+		Message(LogEntry),
+		Action(LogEntry),
+		Warning(LogEntry),
+		Error(LogEntry),
+		Prompt(LogEntry)
 	}
 
-	impl LogEntry<'_> {
-		fn new() -> LogEntry<'static> {
-			let mut result = LogEntry {
-				text: "",
-				direct: FileName::Pathname(""),
-				infix: "",
-				indirect: FileName::Pathname(""),
-				postfix: "",
-			};
-			result
-		}
+	impl LogEntry {
+		// TODO: impl some constructors for LogEntry
+		// fn new() -> LogEntry {
+		// 	let mut result = LogEntry {
+		// 		text: "",
+		// 		direct: FileName::Pathname(""),
+		// 		infix: "",
+		// 		indirect: FileName::Pathname(""),
+		// 		postfix: "",
+		// 	};
+		// 	result
+		// }
 	}
 }
 
@@ -172,125 +184,110 @@ mod types {
 // types: directory filename executable warning error prompt 
 
 pub mod log {
-	use super::TAB;
+	// use super::TAB;
 		use super::BRIGHT::{BLUE, GREEN, YELLOW, CYAN};
 		use super::COLOR::YELLOW as ORANGE;
 		use super::COLOR::GREEN as DARKGREEN;
 		use super::COLOR::{RED, PURPLE};
 		use super::STYLE::BOLD;
 		use super::RESET::ALL as RESET;
-		// use super::RESET::COLOR;
 		use super::types::LogType;
 
 	trait Loggable {
 		fn log(self);
 	}
 
-	impl Loggable for LogType<'static> {
+	impl Loggable for LogType {
 		fn log(self) {
 			use LogType::*;
-			use super::types::LogEntry;
-			use super::RESET::COLOR;
-			let mut inner: LogEntry;
-
-			println!( "{}{}{}: {}{}{}{}{}",
-				TAB, BOLD,
-				if let Header(inner)	= self { BLUE		.to_string() + "[#]" } else
-				if let Info(inner)	 	= self { CYAN		.to_string() + "[*]" } else
-				if let Hint(inner)	 	= self { DARKGREEN	.to_string() + "[+]" } else
-				if let Message(inner)	= self { GREEN		.to_string() + "[>]" } else
-				if let Action(inner)	= self { YELLOW		.to_string() + "[@]" } else
-				if let Warning(inner)	= self { ORANGE		.to_string() + "[!]" } else
-				if let Error(inner)		= self { RED		.to_string() + "[!]" } else
-				if let Prompt(inner)	= self { PURPLE		.to_string() + "[?]" } else
-				{ let inner = {}; COLOR.to_string() + "[_]" } + RESET,
-
-				// FIXME: For some reason, a match expression won't work
-				// FIXME: it refuses to bind the value in the enum to inner
-				// if let prefix = match self {
-				// 	Header(inner)	=> BLUE		.to_string() + "[#]",
-				// 	Info(inner)		=> CYAN		.to_string() + "[*]",
-				// 	Hint(inner)		=> DARKGREEN.to_string() + "[+]",
-				// 	Message(inner)	=> GREEN	.to_string() + "[>]",
-				// 	Action(inner)	=> YELLOW	.to_string() + "[@]",
-				// 	Warning(inner)	=> ORANGE	.to_string() + "[!]",
-				// 	Error(inner)	=> RED		.to_string() + "[!]",
-				// 	Prompt(inner)	=> PURPLE	.to_string() + "[?]",
-				// } + RESET { prefix } else { let inner = COLOR.to_string() + "[_]" + RESET; inner },
-				inner.text,
-				inner.direct,
-				inner.infix,
-				inner.indirect,
-				inner.postfix,
-			)
+			// use super::types::LogEntry;
+			// use super::RESET::COLOR;
+			// FIXME: figure out a PAINLESS way to capture inner from the match self{} expression.
+			match self {
+				Header(inner) => println!( "{}{}{}{}: {}{}{}{}{}",
+					BLUE, BOLD, "[#]", RESET, // :
+					inner.text,
+					inner.direct.to_string(),
+					inner.infix,
+					inner.indirect.to_string(),
+					inner.postfix
+				),
+				Info(inner) => println!( "{}{}{}{}: {}{}{}{}{}",
+					CYAN, BOLD, "[*]", RESET, // :
+					inner.text,
+					inner.direct.to_string(),
+					inner.infix,
+					inner.indirect.to_string(),
+					inner.postfix
+				),
+				Hint(inner) => println!( "{}{}{}{}: {}{}{}{}{}",
+					DARKGREEN, BOLD, "[+]", RESET, // :
+					inner.text,
+					inner.direct.to_string(),
+					inner.infix,
+					inner.indirect.to_string(),
+					inner.postfix
+				),
+				Message(inner) => println!( "{}{}{}{}: {}{}{}{}{}",
+					GREEN, BOLD, "[>]", RESET, // :
+					inner.text,
+					inner.direct.to_string(),
+					inner.infix,
+					inner.indirect.to_string(),
+					inner.postfix
+				),
+				Action(inner) => println!( "{}{}{}{}: {}{}{}{}{}",
+					YELLOW, BOLD, "[@]", RESET, // :
+					inner.text,
+					inner.direct.to_string(),
+					inner.infix,
+					inner.indirect.to_string(),
+					inner.postfix
+				),
+				Warning(inner) => println!( "{}{}{}{}: {}{}{}{}{}",
+					ORANGE, BOLD, "[!]", RESET, // :
+					inner.text,
+					inner.direct.to_string(),
+					inner.infix,
+					inner.indirect.to_string(),
+					inner.postfix
+				),
+				Error(inner) => println!( "{}{}{}{}: {}{}{}{}{}",
+					RED, BOLD, "[!]", RESET, // :
+					inner.text,
+					inner.direct.to_string(),
+					inner.infix,
+					inner.indirect.to_string(),
+					inner.postfix
+				),
+				Prompt(inner) => println!( "{}{}{}{}: {}{}{}{}{}",
+					PURPLE, BOLD, "[?]", RESET, // :
+					inner.text,
+					inner.direct.to_string(),
+					inner.infix,
+					inner.indirect.to_string(),
+					inner.postfix
+				),
+				// This is a catchall for unimplemented message types
+				// Enable if necessary.
+				// LogType::_(inner) => println!( "{}{}{}{}: {}{}{}{}{}",
+				// 	COLOR, BOLD, "[_]", RESET, // :
+				// 	inner.text,
+				// 	inner.direct.to_string(),
+				// 	inner.infix,
+				// 	inner.indirect.to_string(),
+				// 	inner.postfix
+				// ),
+			};
 		}
 	}
-
-	// OLD non-impl logging functions
-	// use super::types::FileName
-	// pub fn head<T>(header: FileName<&str>) {
-	// 	println!( "{}{}[#]{}: {}",
-	// 		super::TAB,
-	// 		BLUE,
-	// 		RESET,
-	// 		<FileName<&str> as Into<T>>::into(header.to_str())
-	// 	)
-	// }
-	// pub fn message<T>(message: FileName<&str>) {
-	// 	println!( "{}{}[>]{}: {}",
-	// 		super::TAB,
-	// 		GREEN,
-	// 		RESET,
-	// 		<FileName<&str> as Into<T>>::into(message.to_str())
-	// 	)
-	// }
-	// pub fn warn<T>(warning: FileName<&str>) {
-	// 	eprintln!( "{}{}[!]{}: {}",
-	// 		super::TAB,
-	// 		ORANGE,
-	// 		RESET,
-	// 		<FileName<&str> as Into<T>>::into(warning.to_str())
-	// 	)
-	// }
-	// pub fn enter_dir(directory: FileName<&str>) {
-	// 	println!( "{}{}[@]{}: Entering {}",
-	// 		super::TAB,
-	// 		YELLOW,
-	// 		RESET,
-	// 		super::STYLE::DIR.to_string() + directory.to_str()
-	// 	)
-	// }
-	// pub fn leave_dir(directory: FileName<&str>) {
-	// 	println!( "{}{}[@]{}: Leaving {}",
-	// 		super::TAB,
-	// 		super::BRIGHT::YELLOW,
-	// 		RESET,
-	// 		super::STYLE::DIR.to_string() + directory.to_str()
-	// 	)
-	// }
-	// pub fn err<T>(error: FileName<&str>) {
-	// 	eprintln!( "{}{}[!]{}: {}",
-	// 		super::TAB,
-	// 		RED,
-	// 		RESET,
-	// 		<FileName<&str> as Into<T>>::into(error.to_str())
-	// 	)
-	// }
-	// pub fn prompt<T>(prompt: FileName<&str>) {
-	// 	println!( "{}{}[?]{}: {}",
-	// 		super::TAB,
-	// 		PURPLE,
-	// 		RESET,
-	// 		<FileName<&str> as Into<T>>::into(prompt.to_str())
-	// 	);
-	// }
 }
 
-#[test]
-fn check_dirname() {
-	// use types::FileName;
-	println!("Tests:\n{} {}\n{} {}\n",
-		"dir", "dir".to_directory(),
-		"dir/", "dir/".to_directory(),
-	)
-}
+// #[test]
+// fn check_dirname() {
+//	use types::FileName;
+// 	println!("Tests:\n{} {}\n{} {}\n",
+// 		"dir", "dir".to_directory(),
+// 		"dir/", "dir/".to_directory(),
+// 	)
+// }
